c41d94fcc311ba7d59f8e1d9b79e88b0469e38cd
filtercake <fabian@superpursuitmo.de>
2017-03-13 14:40:08 +0100

    WRP™: correct body height

--- bucket/main.js
+++ bucket/main.js
@@ -1,4 +1,81 @@
 
+
+
+  window.addEventListener("load", function(event) {
+    console.log("All resources finished loading!");
+    // deployTheSticky();
+    // setInitialHeigh ();
+
+
+  });
+
+
+
+
+function deployTheSticky () {
+  console.log("whoop");
+
+
+
+
+  // https://remysharp.com/drafts/smooth-scroll-with-sticky-nav
+
+// get the sticky element with the id of `sticky-header`.
+
+
+// var d = document.getElementById("sticky-header-container");
+// var topPos = d.offsetTop;
+
+// var referenceOffset = topPos;
+
+// var $stickyHeaderRef = $('#sticky-header-container');
+
+// console.log(referenceOffset);
+
+// then record the current position, so when we cross the
+// boundary the `sticky` class can be toggled
+// var boundary = $stickyHeaderRef.position().top;
+
+var boundary = 200;
+console.log(boundary);
+
+// when the page scrolls, do as little as possible, in this
+// case we're just registering a rAF callback to `checkSticky`
+window.onscroll = function (event) {
+  requestAnimationFrame(checkSticky);
+};
+
+function checkSticky() {
+  // collect current scroll position, with a arbitrary amount
+  // of inertia.
+  var y = window.scrollY + 2;
+
+  // check if the element contains the `sticky` class already
+  var isSticky = document.body.classList.contains('sticky');
+
+  console.log(isSticky);
+  console.log(document.body.classList);
+
+    if (y > boundary) {
+    // if we're in the "sticky" boundary, and it's not already
+    // sticky, then apply the class, otherwise do nothing.
+    console.log("yes");
+    if (!isSticky) {
+ console.log(document.body.classList);     document.body.classList.add('sticky');
+    }
+  } else if (isSticky) {
+    // otherwise, we're inside the region *and* the sticky
+    // class needs to be removed.
+    document.body.classList.remove('sticky');
+  }
+
+
+}
+
+}
+
+
+
 // ***********************
 // notes: persist to localstoreage
 
@@ -34,8 +111,8 @@ window.addEventListener('hashchange', function() {
 // <div id="debug-console" style="position: fixed; right: 1em; bottom: 5em;"></div>
 
 function debug (val) {
-  document.getElementById("debug-console").innerHTML = '<div style="padding: 1em; background-color: yellow; border: 3px dotted black"><code>' + val + '</code></div>';
-  console.log(val);
+  // document.getElementById("debug-console").innerHTML = '<div style="padding: 1em; background-color: yellow; border: 3px dotted black"><code>' + val + '</code></div>';
+  // console.log(val);
 }
 
 
